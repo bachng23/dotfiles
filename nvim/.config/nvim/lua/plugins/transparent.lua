99fe775af8c0a1fb30045b4d69a122cae2fc9928
bachng23 <bachnguyen2326@gmail.com>
2026-05-11 11:04:27 +0800

    Fix: Enable transparency for Neo-tree

--- nvim/.config/nvim/lua/plugins/transparent.lua
+++ nvim/.config/nvim/lua/plugins/transparent.lua
@@ -14,9 +14,14 @@ return {
         "FzfLuaPreviewBorder",
         "FzfLuaPreviewNormal",
         "FzfLuaPreviewTitle",
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+        "NeoTreeWinSeparator",
+        "NeoTreeEndOfBuffer",
       },
     })
     require("transparent").clear_prefix("lualine")
+    require("transparent").clear_prefix("NeoTree")
     vim.cmd("highlight Normal guibg=NONE")
     vim.cmd("highlight Lualine guibg=NONE")
     vim.cmd("highlight Lualine guifg=NONE")
